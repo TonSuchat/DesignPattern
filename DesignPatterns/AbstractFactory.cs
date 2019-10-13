0744541ba6e516c1f4add0a50a772b6b7feabfdd
TonSuchat <suchat.v26@gmail.com>
2019-10-13 19:17:15 +0700

    Initial project along with first design pattern AbstractFactory

---
+++ DesignPatterns/AbstractFactory.cs
@@ -0,0 +1,75 @@
+﻿namespace ConsoleApp.DesignPatterns
+{
+
+    public class AbstractFactory
+    {
+        // products
+        public interface IButton
+        {
+            string Clicked();
+        }
+
+        public interface ICheckbox
+        {
+            string Checked();
+        }
+
+        public class Button : IButton
+        {
+            public string OSName { get; set; }
+
+            public string Clicked()
+            {
+                return $"{OSName} button has been clicked";
+            }
+        }
+        public class Checkbox : ICheckbox
+        {
+            public string OSName { get; set; }
+
+            public string Checked()
+            {
+                return $"{OSName} checkbox has been checked";
+            }
+        }
+
+        // factory
+        public interface GUIFactory
+        {
+            Button createButton();
+            Checkbox createCheckbox();
+        }
+
+        // window factory (windows os implementation version of GUIFactory)
+        public class WinFactory : GUIFactory
+        {
+            private readonly string osName = "Windows";
+
+            public Button createButton()
+            {
+                return new Button() { OSName = osName };
+            }
+
+            public Checkbox createCheckbox()
+            {
+                return new Checkbox() { OSName = osName };
+            }
+        }
+
+        // mac factory (mac os implementation version of GUIFactory)
+        public class MacFactory : GUIFactory
+        {
+            private readonly string osName = "Mac";
+            public Button createButton()
+            {
+                return new Button() { OSName = osName };
+            }
+
+            public Checkbox createCheckbox()
+            {
+                return new Checkbox() { OSName = osName };
+            }
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using static ConsoleApp.DesignPatterns.AbstractFactory;
+
+namespace DesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DemoAbstractFactory();
+            Console.ReadLine();
+        }
+
+        #region AbstractFactory
+        /// <summary>
+        /// There're button, checkbox for 2 OS(Windows, Mac) and it has difference implementations, So use AbstractFactory design pattern for solving and demo this problem.
+        /// </summary>
+        public static void DemoAbstractFactory()
+        {
+            Console.WriteLine($"================AbstractFactory================{Environment.NewLine}");
+            var winFactory = new WinFactory();
+            var macFactory = new MacFactory();
+
+            var winButton = winFactory.createButton();
+            var macButton = macFactory.createButton();
+
+            Console.WriteLine(winButton.Clicked());
+            Console.WriteLine(macButton.Clicked());
+
+            var winCheckbox = winFactory.createCheckbox();
+            var macCheckbox = macFactory.createCheckbox();
+
+            Console.WriteLine(winCheckbox.Checked());
+            Console.WriteLine(macCheckbox.Checked());
+            Console.WriteLine($"{Environment.NewLine}================AbstractFactory================");
+        }
+        #endregion
+
+    }
+
+}
+
