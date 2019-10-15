ac9346048af9d92ee1e0338e37d866b00781ae2c
TonSuchat <suchat.v26@gmail.com>
2019-10-15 10:28:14 +0700

    Implement Bridge pattern

---
+++ DesignPatterns/Adapter.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace DesignPattern.DesignPatterns
+{
+    public class Adapter : Pattern
+    {
+        // Class that client used in application
+        public class Hero
+        {
+            public Hero() { }
+
+            public string Name { get; set; }
+            public string Attribute { get; set; }
+
+            public string GetInfo()
+            {
+                return $"Name: {Name}, Attribute: {Attribute}";
+            }
+        }
+
+        // Actual Service that provide data as JSON
+        public class HeroJSONAPI
+        {
+            public string Hero()
+            {
+                Dictionary<string, object> json = new Dictionary<string, object>() { { "Name", "Axe" }, { "Attribute", "Strength" } };
+                return JsonConvert.SerializeObject(json);
+            }
+        }
+
+        // Adapter & Interface
+        public interface IHeroAdapter
+        {
+            Hero GetHero();
+        }
+
+        public class HeroJSONAdapter : IHeroAdapter
+        {
+            public Hero GetHero()
+            {
+                var api = new HeroJSONAPI();
+                Dictionary<string, object> json = JsonConvert.DeserializeObject<Dictionary<string, object>>(api.Hero());
+                var hero = new Hero() { Name = json["Name"].ToString(), Attribute = json["Attribute"].ToString() };
+                return hero;
+            }
+        }
+
+        /// <summary>
+        /// Problem: When we need to add some implement without touching existing code
+        /// Solved: Use the interface for adapt new service to actual result
+        /// </summary>
+        public override void Demo()
+        {
+            IHeroAdapter adapter = new HeroJSONAdapter();
+            var hero = adapter.GetHero();
+            Console.WriteLine(hero.GetInfo());
+        }
+    }
+}
---
+++ DesignPatterns/Bridge.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace DesignPattern.DesignPatterns
+{
+    public class Bridge : Pattern
+    {
+        // Abstraction
+        public class Remote
+        {
+            private readonly IDevice _device;
+            public Remote(IDevice device) => _device = device;
+
+            public void TogglePower()
+            {
+                if (_device.IsActive) _device.TurnOff();
+                else _device.TurnOn();
+            }
+            public void IncreaseVolumn() => _device.SetVolumn(_device.Volumn + 1);
+            public void DecreaseVolumn() => _device.SetVolumn(_device.Volumn - 1);
+            public void IncreaseChannel() => _device.SetChannel(_device.Channel + 1);
+            public void DecreaseChannel() => _device.SetChannel(_device.Channel - 1);
+        }
+
+        // Implementation
+        public interface IDevice
+        {
+            bool IsActive { get; set; }
+            int Channel { get; set; }
+            int Volumn { get; set; }
+            void TurnOn();
+            void TurnOff();
+            int GetVolumn();
+            void SetVolumn(int volumn);
+            int GetChannel();
+            void SetChannel(int channel);
+        }
+
+        // concrete implementation
+        public class Television : IDevice
+        {
+            public bool IsActive { get; set; }
+            public int Channel { get; set; }
+            public int Volumn { get; set; }
+            public Television()
+            {
+                IsActive = false;
+                Channel = 1;
+                Volumn = 1;
+            }
+
+            public int GetChannel() => Channel;
+
+            public int GetVolumn() => Volumn;
+
+            public void SetChannel(int channel) => Channel = channel;
+
+            public void SetVolumn(int volumn) => Volumn = volumn;
+
+            public void TurnOff() => IsActive = false;
+
+            public void TurnOn() => IsActive = true;
+        }
+
+        // client
+        /// <summary>
+        /// Problem: When we have 2 independent and we need to working with both of them
+        /// Solved: We changed from inheritance to composition use Bridge pattern for connect both of them
+        /// </summary>
+        public override void Demo()
+        {
+            IDevice television = new Television();
+            Remote remote = new Remote(television);
+            Console.WriteLine($"Turned on: {television.IsActive}, Volumn: {television.Volumn}, Channel: {television.Channel}");
+            remote.TogglePower();
+            remote.IncreaseChannel();
+            remote.IncreaseVolumn();
+            Console.WriteLine($"Turned on: {television.IsActive}, Volumn: {television.Volumn}, Channel: {television.Channel}");
+        }
+
+    }
+}
